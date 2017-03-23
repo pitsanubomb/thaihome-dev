73e3f8fa530181dea3e29b64374685d0a36af14b
Tigran Asatryan <tigran9977@gmail.com>
2017-03-24 00:26:08 +0400

    password for node helper

--- controllers/InvoiceController.js
+++ controllers/InvoiceController.js
@@ -1,4 +1,5 @@
 var Invoice = require('../models/invoice');
+var Bookings = require('../models/Booking');
 
 exports.getInvoicesBayDateRange = function (req, res) {
     Invoice.find({
@@ -8,9 +9,55 @@ exports.getInvoicesBayDateRange = function (req, res) {
         }
     }, function (err, invoices) {
         if (!err) {
-            res.json({error: false, data: invoices});
+            var bookingIds = [];
+
+            for (var i = 0; i < invoices.length; i++) {
+                bookingIds.push(invoices[i].bookingId);
+            }
+            Bookings.aggregate(
+                {
+                    $lookup: {
+                        from: "users",
+                        localField: "user",
+                        foreignField: "_id",
+                        as: "userData"
+                    }
+                }, {
+                    $match: {
+                        $and: [{"_id": {$in: bookingIds}}]
+                    }
+                }, {$project: {_id: 1, userData: 1}}
+
+                , function (err, users) {
+                    if(!err){
+                        var inv = [];
+                        for (var q = 0; q < invoices.length; q++){
+                            var current = users.filter(function(obj){
+                                return obj._id == invoices[q].bookingId;
+                            });
+                            inv.push({
+                                invoiceNumber: invoices[q].invoiceNumber,
+                                bookingId: invoices[q].bookingId,
+                                propertyId: invoices[q].propertyId,
+                                createDate: invoices[q].createDate,
+                                dueDate: invoices[q].dueDate,
+                                paidDate: invoices[q].paidDate,
+                                paymentSuggest: invoices[q].paymentSuggest,
+                                managerId: invoices[q].managerId,
+                                invoiceLines: invoices[q].invoiceLines,
+                                includeMissing: invoices[q].includeMissing,
+                                username: current[0].userData[0].name
+                            });
+                            if(q == invoices.length - 1){
+                                res.json({error:false,data:inv});
+                            }
+                        }
+                    }else{
+                        res.json({error:true});
+                    }
+                })
         } else {
             res.json({error: true, message: err});
         }
     })
-}
+};
